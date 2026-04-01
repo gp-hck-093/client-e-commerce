67d1ce5c6c0523bb9335184e7484328f97eb7f06
erwindW99 <erwindwiranata17@gmail.com>
2026-04-01 17:09:29 +0700

    chore: change api

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
+  // baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
+  baseURL:
+    import.meta.env.VITE_API_URL || "https://56hgb32x-3000.asse.devtunnels.ms/",
 });
 
 export default api;
